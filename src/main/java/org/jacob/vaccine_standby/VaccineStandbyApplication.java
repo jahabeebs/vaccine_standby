16cee44b09a3110c32ad8d3f2a408fc1c823e944
Jacob Habib <habib.50@osu.edu>
2021-01-31 15:55:40 -0500

    Vaccine standby first commit

---
+++ src/main/java/org/jacob/vaccine_standby/VaccineStandbyApplication.java
@@ -0,0 +1,13 @@
+package org.jacob.vaccine_standby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineStandbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccineStandbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jacob/vaccine_standby/VaccineStandbyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jacob.vaccine_standby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineStandbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
